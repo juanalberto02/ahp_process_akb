eb914958fb18494a347cfbe02223e6839632b3c3
Crufixzz <165782830+Crufixzz@users.noreply.github.com>
2024-04-03 01:57:20 +0700

    Add files via upload

--- uasahp.py
+++ uasahp.py
@@ -4,7 +4,24 @@ import numpy as np
 from io import StringIO
 import plotly.express as px
 
-
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #1E1E1E;
+            color: white;
+        }
+        .sidebar .sidebar-content {
+            background-color: #333333;
+            color: white;
+        }
+        .Widget>label {
+            color: white;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 # Fungsi untuk perhitungan AHP
 def ahp_attributes(ahp_df):
     sum_array = np.array(ahp_df.sum(numeric_only=True))
